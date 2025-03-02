f2f7b58b07e99e6e605cf0de018e35e9c3fd0d81
Víctor Mejía <vmejiaec@gmail.com>
2025-03-01 19:10:37 -0500

    Uso de paquete Figuras-Lib

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
 import json
-from figuras.cuadrado import Cuadrado
-from figuras.rectangulo import Rectangulo
-from figuras.circulo import Circulo
-
-# Mapeo de tipos a clases
-TIPOS_FIGURAS = {
-    'Cuadrado': Cuadrado,
-    'Rectangulo': Rectangulo,
-    'Circulo': Circulo
-}
+from figuras import FabricaFiguras
 
 # Leer datos desde JSON
 def cargar_datos():
-    with open('datos.json') as file:
-        return json.load(file)
-    
-# Crear lista de figuras
-def crear_figuras(datos):
-    figuras = []
-    for dato in datos:
-        tipo = dato.pop('tipo')
-        clase = TIPOS_FIGURAS[tipo]
-        if clase:
-            figuras.append(clase(**dato))
-    return figuras
+    try:
+        with open('datos.json') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print('No se encontro el archivo datos.json')
+        return []
+    except json.JSONDecodeError:
+        print('Error al leer el archivo datos.json')
+        return []
 
 # main
 if __name__ == "__main__":
     datos = cargar_datos()
-    figuras = crear_figuras(datos)
+    figuras = FabricaFiguras.crear_figuras(datos)
     for figura in figuras:
         print(figura)
         print(f'Area: {figura.area()}')
